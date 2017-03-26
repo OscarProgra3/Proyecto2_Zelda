7dceb998ffb77bc8d7ee622e6bba75edba5ae3cd
OscarProgra3 <Daniel.embu96nitec.edu>
2017-03-26 17:08:35 -0600

    actualizando

---
+++ Main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include <conio.h>
+
+#include <fstream>//para archivos binarios
+
+using namespace std;
+
+
+string dibujolink();
+
+int main()
+{
+  string zelda;
+  system(color Oa);
+  cout<<dibujolink()+"";
+  cin>>zelda;
+
+  return 0;
+}
+
+string dibujolink()
+{
+  
+string linkp="      .---_   _--._---.__ \n    /   _ \\/\\     \\     ''-. \n   |   / \\ | \\      '.     '.     .'¯| \n   |  / __\\|  '.      '.     '    \\ _| \n   | | |_  .-'_'.      .    _.-|  $$ \n   | | \\U\\ .¯U' '..    _.-' .'  $$ \n    \\| /¯ /  ¯¯     ' |'   .'|   $$ \n      \\| '-          '|  .'  |  $$ \n        \\ --         /.      \\ $$ \n         \\          .'   __   |$$ \n          '.   ___.'  | ////¯¯''.._ \n            ¯|¯      _'.//_ ..   |||| \n       _ .-¯|\\|----¯¯¯  |\\|¯¯|---|||| \n    .-¯.'  |  X      .''  \\  |       ''. \n   '    ''..      .''__... |/           '. \n   | |    .|   .' .-   / \\|              | \n  /  '      '.'...'   /  /               | \n /_ |        '       /  /     |          | \n  |¯|                 '  '     \\| __.-.___.' \n  | |              /  /        |\\         \\  \n  | .             /  /           |        . \n  | '            '  '            |        ' \n   \\ .        /  /               '        | \n   /  '      '  '                 |       \\ \n  /   '                           |        |";
+  return linkp;
+}
